7ee38b185148a3a7444253563db62ce6defcc0ae
Mike Herceg <mikehercegdesigns@gmail.com>
2022-06-03 11:33:11 -0400

    built a crappy stop watch with vue

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,25 @@ import {ref, watch} from 'vue'
 
 const count = ref<string>('00:00:00')
 const isCounting = ref<boolean>(false)
+const timeStamps = ref<string[]>([])
 
-
-const handleClick = () =>{ 
+const toggleStart = () =>{ 
   isCounting.value = !isCounting.value
 }
 
+const handleReset = () =>{
+  isCounting.value = false
+  count.value = '00:00:00'
+}
+
+const handleTimeStamp = () =>{
+  console.log(timeStamps.value)
+ timeStamps.value.push(count.value)
+}
+
+const removeTimeStamp = (timeStamp:string) =>{
+  timeStamps.value = timeStamps.value.filter(stamp => stamp !== timeStamp)
+}
 
 let hr = 0;
 let min = 0;
@@ -53,13 +66,51 @@ watch(isCounting, timerCycle)
 
 <template>
   <main>
-     {{count}}
-    <PrimaryButton :onClick="handleClick">
-      {{ isCounting ? 'stop' : 'start' }}
-    </PrimaryButton>
+    <div class="timer">
+        <h1>
+        {{count}}
+        </h1>
+        <PrimaryButton :onClick="toggleStart">
+          {{ isCounting ? 'stop' : 'start' }}
+        </PrimaryButton>
+        <PrimaryButton :onClick="handleReset">
+          reset
+        </PrimaryButton>
+    </div>
+    <div v-if="isCounting || timeStamps.length" class="time-stamps">
+        <div  v-if="timeStamps.length">
+        <h2>time stamps</h2>
+        <ul >
+          <li v-for="timeStamp in timeStamps" :key="timeStamp">
+            {{ timeStamp }} 
+            <PrimaryButton :onClick="()=>removeTimeStamp(timeStamp)">
+              remove
+            </PrimaryButton>
+          </li>
+        </ul>
+        </div>
+       
+         <PrimaryButton :onClick="handleTimeStamp" v-if="isCounting">
+          time Stamp 
+        </PrimaryButton>
+    </div>
+    
+    
   </main>
 </template>
 
 <style lang="scss">
   @import './assets/base.scss';
+  body{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    main {
+      display: flex;
+      align-items: center;
+      *{
+        margin: 10px
+      }
+    }
+  }
 </style>
